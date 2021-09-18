da13f8991b4576d722536e89d418529c7d255989
albertduranll <alduli@floridauniversitaria.es>
2021-09-18 18:44:56 +0200

    AE1-2. Ejercicio 2.b

--- PSP_AlbertDuran/src/es/florida/PSP_AlbertDuran/App.java
+++ PSP_AlbertDuran/src/es/florida/PSP_AlbertDuran/App.java
@@ -1,11 +1,15 @@
 package es.florida.PSP_AlbertDuran;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
 
 	public static void main(String[] args) {
 		
 //		sayHello();
-		imprimirArrayAlumnos();
+//		imprimirArrayAlumnos();
+		imprimirListaAlumnos();
 	}
 	
 	public static void sayHello() {
@@ -26,5 +30,20 @@ public class App {
 		for(int i = 0; i < arrayAlumnos.length; i++) {
 			System.out.println(arrayAlumnos[i]);
 		}
+	}
+	
+	public static void imprimirListaAlumnos() {
+		
+		 List<String> listAlumnos = new ArrayList<String>(); 
+		 
+		 listAlumnos.add("Albert");
+		 listAlumnos.add("Laura");
+		 listAlumnos.add("Alvaro");
+		 listAlumnos.add("Clara");
+		 listAlumnos.add("Xavi");
+		 listAlumnos.add("Cristina");
+		 
+		 for(String alumno:listAlumnos)  
+			  System.out.println(alumno);  
 	}
 }
